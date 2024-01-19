1e9566f6a93f40b5e3fc0b3362181b3b918b16f8
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-19 19:05:20 +0200

    request body

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,6 +9,7 @@ const getContacts = (req, res) => {
 //@route POST /api/contacts
 //@access public
 const createContact = (req, res) => {
+  console.log("The request body are:", req.body);
   res.status(201).json({ message: "Create contact" });
 };
 
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000; // define a port
 
-app.use("/api/contacts", require("./routes/contactRoutes"));
+// middleware
+app.use(express.json()); // all user to send  data request body
+app.use("/api/contacts", require("./routes/contactRoutes")); // routing
 
+// listening to the port
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
