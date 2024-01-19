ec2bde824ab0dcc249d644c30e9a1b19e6991314
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-19 18:20:57 +0200

    create all routes

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000; // define a port
 
-app.get("/api/contacts", (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
