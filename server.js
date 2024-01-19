ee6f14eda82f9d276bfa7542f224cfa4326a0f24
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-19 18:03:46 +0200

    create first route

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("running....");
+const express = require("express"); //creating express server
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000; // define a port
+
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
