e199a2d758549bb41c0b89aa1e968530ac97f36a
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-28 17:31:13 +0200

    login api and get token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../model/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 //desc Register a user
 //@route POST /api/users/register
 //@access public
@@ -42,7 +43,31 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access public
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  
+  const user = await User.findOne({ email });
+  // compare password with hashedPassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
 });
 
 //desc current user information
