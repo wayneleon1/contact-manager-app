c7d760b3a87ba717ce11c88c2da08bdd5e88394a
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-31 21:27:29 +0200

    get only  contact created by user_id

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,7 @@ const Contact = require("../model/contactModel");
 //@route GET /api/contacts
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find({ user_id: req.user.id });
   res.status(200).json({ contacts });
 });
 
@@ -24,6 +24,7 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
+    user_id: req.user.id,
   });
 
   res.status(201).json({ contact });
