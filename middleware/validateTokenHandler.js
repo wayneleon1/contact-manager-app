8ef35f8b6fdc15811b185f8a6618065dcc924081
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-31 19:15:45 +0200

    Fix error

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
   let authHeader = req.headers.Authorization || req.headers.authorization;
-  if (authHeader && authHeader.startWith("Bearer")) {
+  if (authHeader && authHeader.startsWith("Bearer")) {
     token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
       if (err) {
